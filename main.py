9bc67f7a7b47d0735ffbd52960fb1abc8f619561
Christopher Ciollaro <cciollaro@gmail.com>
2021-05-27 19:39:30 -0400

    jffjfj

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import hotplots
-import time
-import yaml
 import logging
+import time
 
-from hotplots_config import HotplotsConfig
+import yaml
+
 from hotplots import Hotplots
+from hotplots_config import HotplotsConfig
 
-# move this directory to my plotter (for development):
-# rsync -alPvz /Users/cciollaro/PycharmProjects/hotplots cc@cc-desktop-linux.local:/home/cc
-# rm -rf venv
-# python3 -m venv venv
-# . ./venv/bin/activate
-# pip install -r requirements.txt
-# python main.py
 if __name__ == '__main__':
     logging.basicConfig(filename='hotplots.log', level=logging.DEBUG) # TODO: figure out why encoding='utf-8' doesn't work on linux/py3.8
 
